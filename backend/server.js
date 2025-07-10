0beacec84e0694889b06c61ff6b0d531172c33db
Shweta01-Beladiya <shweta.kalathiyainfotech@gmail.com>
2025-07-10 16:43:27 +0530

    shweta10-07

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-import React from 'react'
+// import React from 'react'
 
-export default function server() {
-  return (
-    <div>server</div>
-  )
-}
+// export default function server() {
+//   return (
+//     <div>server</div>
+//   )
+// }
